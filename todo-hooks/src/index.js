cc98b75f16f5112fce2be448ef706b26261a6041
Andrey Alekseev <aandrewaleks@gmail.com>
2019-06-02 13:58:15 +0200

    added control input field

--- todo-hooks/src/index.js
+++ todo-hooks/src/index.js
@@ -5,18 +5,21 @@ import * as serviceWorker from "./serviceWorker";
 
 const App = props => {
   const [count, setCount] = useState(props.count);
-
+  const [text, setText] = useState("");
   //   const increment = () => {
   //     return setCount(count + 1);
   //   };
   return (
     <div>
-      <p>The currecnt count is: {count}</p>
+      <p>
+        The currecnt {text || "count"} is: {count}
+      </p>
       {/* <button onClick={increment}>+1</button> */}
 
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(props.count)}>Reset</button>
       <button onClick={() => setCount(count + 1)}>+1</button>
+      <input value={text} onChange={e => setText(e.target.value)} />
     </div>
   );
 };
