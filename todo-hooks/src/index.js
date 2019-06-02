fcafb4c3f4fb576ea453c554fdb937754e827ece
Andrey Alekseev <aandrewaleks@gmail.com>
2019-06-02 13:27:19 +0200

    simpified create-reat-app to index.js to explore hooks easier

--- todo-hooks/src/index.js
+++ todo-hooks/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+const App = () => {
+  return (
+    <div>
+      <p>The currecnt count is 0</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
