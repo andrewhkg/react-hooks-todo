10ba8730bd627c6ce598c39ff692cc31e12009f5
Andrey Alekseev <aandrewaleks@gmail.com>
2019-06-02 13:47:40 +0200

    set up hook useState to increment, dicrement and reset count

--- todo-hooks/src/index.js
+++ todo-hooks/src/index.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 import * as serviceWorker from "./serviceWorker";
 
-const App = () => {
+const App = props => {
+  const [count, setCount] = useState(props.count);
+
+  //   const increment = () => {
+  //     return setCount(count + 1);
+  //   };
   return (
     <div>
-      <p>The currecnt count is 0</p>
+      <p>The currecnt count is: {count}</p>
+      {/* <button onClick={increment}>+1</button> */}
+
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+App.defaultProps = {
+  count: 0
+};
+
+ReactDOM.render(<App count={22} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
